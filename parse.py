9774270a6bf61622a24e28e549354d850ab52766
David Batley <git@dbatley.com>
2021-05-05 22:46:23 +0100

    Add labels automatically

--- parse.py
+++ parse.py
@@ -27,15 +27,33 @@ for region_name, region_count in regions.items():
 with open("map.template") as f:
     template = f.read()
 
-
+percentfmt = lambda value: "%d%%" % int(round(value * 100))
 imagescolor = lambda value: "hsl(30, %d%%, 50%%)" % max(min((value * 300 - 50), 100), 0)
 with open("map-images.svg", "w") as f:
-    f.write(template % {
-        region_name.replace(" (England)", ""): imagescolor(region_count["image_url"] / float(region_count["candidates"]))
-        for region_name, region_count in regions.items()})
+    data = {
+        "title1": "Percentage of",
+        "title2": "candidates with",
+        "title3": "images",
+        "title4": "",
+    }
+    for region_name, region_count in regions.items():
+        name = region_name.replace(" (England)", "")
+        ratio = region_count["image_url"] / float(region_count["candidates"])
+        data[name] = imagescolor(ratio)
+        data["text" + name] = percentfmt(ratio)
+    f.write(template % data)
 
 infocolor = lambda value: "hsl(200, %d%%, 50%%)" % max(min((value * 300 - 50), 100), 0)
 with open("map-has-info.svg", "w") as f:
-    f.write(template % {
-        region_name.replace(" (England)", ""): infocolor(region_count["can_get_info"] / float(region_count["candidates"]))
-        for region_name, region_count in regions.items()})
+    data = {
+        "title1": "Percentage of",
+        "title2": "candidates with",
+        "title3": "social media",
+        "title4": "or email",
+    }
+    for region_name, region_count in regions.items():
+        name = region_name.replace(" (England)", "")
+        ratio = region_count["can_get_info"] / float(region_count["candidates"])
+        data[name] = infocolor(ratio)
+        data["text" + name] = percentfmt(ratio)
+    f.write(template % data)
